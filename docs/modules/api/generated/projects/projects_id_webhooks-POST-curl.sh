d7710f035ffe315f13cc3edc8a81a22b6d27c9b8
Albert Ziegenhagel <albert.ziegenhagel@outlook.com>
2022-02-15 11:40:38 +0100

    Generate webhook API examples

---
+++ docs/modules/api/generated/projects/projects_id_webhooks-POST-curl.sh
@@ -0,0 +1,9 @@
+curl -X POST \
+  --header "Content-Type: application/json" \
+  --data '{
+             "name": "My Webhook",
+             "url": "http://example.com/my/hook/path",
+             "secret_token": "sMeyqOYenB8Czi2zGOFHNg",
+             "on_submission": false
+         }' \
+  http://dtf.example.com/api/projects/1/webhooks
---
+++ docs/modules/api/generated/projects/projects_id_webhooks_id-PUT-curl.sh
@@ -0,0 +1,14 @@
+curl -X PUT \
+  --header "Content-Type: application/json" \
+  --data '{
+             "project": 1,
+             "id": 1,
+             "name": "Demo Bot",
+             "url": "http://129.26.133.164:8666/references/demo",
+             "secret_token": "58a61681-f3c0-4d0f-a8bf-c87193f2603d",
+             "on_submission": true,
+             "on_test_result": false,
+             "on_reference_set": false,
+             "on_test_reference": true
+         }' \
+  http://dtf.example.com/api/projects/1/webhooks/1
