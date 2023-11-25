6a165bab9570845f6457bae25c97ce9b7def01da
Albert Ziegenhagel <albert.ziegenhagel@outlook.com>
2023-11-25 15:24:53 +0100

    Generate submission tests API examples

---
+++ docs/modules/api/generated/test_results/projects_id_submissions_id_tests-POST-empty-curl.sh
@@ -0,0 +1,6 @@
+curl -X POST \
+  --header "Content-Type: application/json" \
+  --data '{
+             "name": "New Empty Test"
+         }' \
+  http://dtf.example.com/api/projects/1/submissions/1/tests
---
+++ docs/modules/api/generated/test_results/projects_id_submissions_id_tests-POST-nonempty-curl.ps1
@@ -0,0 +1,17 @@
+curl -X POST `
+  --header "Content-Type: application/json" `
+  --data '{
+             ""name"": ""New Test"",
+             ""results"": [
+                 {
+                     ""name"": ""runtime"",
+                     ""value"": {
+                         ""data"": ""PT0.124356S"",
+                         ""type"": ""duration""
+                     },
+                     ""status"": ""unknown"",
+                     ""reference"": null
+                 }
+             ]
+         }' `
+  http://dtf.example.com/api/projects/1/submissions/1/tests
